e4b84cb71a13ff8cbecc1d8e7f308e67b25d2590
NpatPanya <npatonpym@gmail.com>
2024-12-06 11:46:47 +0700

    WSDL update 2

--- src/main/java/org/acme/SOAP_service/AllinOne/AllInOneService.java
+++ src/main/java/org/acme/SOAP_service/AllinOne/AllInOneService.java
@@ -1,6 +1,7 @@
 package org.acme.SOAP_service.AllinOne;
 
 import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
 
 import java.util.ArrayList;
@@ -13,6 +14,16 @@ import org.acme.SOAP_service.AllinOne.material.OBJD;
 @WebService(name = "Dataset" , serviceName = "Dataset")
 public interface AllInOneService {
 
-    @WebMethod
-    Wrapper combineStructure(String type, Date date, ArrayList<String> sign, OBJD objd, OBJCollection collection );
+//    @WebMethod
+//    Wrapper combineStructure(String type, Date date, ArrayList<String> sign, OBJD objd, OBJCollection collection );
+//}
+
+        @WebMethod
+        Wrapper combineStructure(
+                @WebParam(name = "type") String type,
+                @WebParam(name = "date") Date date,
+                @WebParam(name = "sign") ArrayList<String> sign,
+                @WebParam(name = "objd") OBJD objd,
+                @WebParam(name = "collection") OBJCollection collection
+        );
 }
